956d22052af6469ef5d080299d935ac24e240d5a
Bruno Goniadis <brunogoniadislima@gmail.com>
2023-01-05 20:14:48 -0300

    feat: DescriptionOnPropsCard

---
+++ src/components/CardContainer/index.tsx
@@ -0,0 +1,7 @@
+
+
+export const CardContainer = () =>{
+    return(
+
+    )
+}
--- src/pages/Tecnologias/index.tsx
+++ src/pages/Tecnologias/index.tsx
@@ -1,13 +1,25 @@
 import { Container } from "./styles";
-import { Card } from "../../components/Card";
 
+import { Card } from "../../components/Card";
+import react from "../../assets/react.svg"
 
 export const Tecnologias = () =>{
 
     return(
         <Container>
 
-         <Card></Card>
+        
+
+         <Card 
+            img={react}
+            title="ReactJs" 
+            type1="Framework" 
+            type2="Front-End" 
+            description=
+            "Linguagem single page"
+         ></Card>
+
+
         </Container>
     )
 

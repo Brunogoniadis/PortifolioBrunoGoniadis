c0606657975c1ac384ba017885c6047b7cd03603
Bruno Goniadis <brunogoniadislima@gmail.com>
2023-01-06 02:17:15 -0300

    logic: cardState text

--- src/components/CardContainer/index.tsx
+++ src/components/CardContainer/index.tsx
@@ -1,22 +1,54 @@
+import { useState , useEffect } from "react"
+
+
 import { ContainerBaseCards } from "./styles"
 import { ICardContainer } from "./types"
 
 
 export const CardContainer = ({card, card2, card3}:ICardContainer) =>{
+    
+    const [textTec, setTextTec] = useState({
+        title:'Clique em um botão', 
+        description:'Para saber da minha jornada'
+    });
+
+    
+    function ReactJs (){
+        setTextTec({
+            title:"ReactJs",
+            description:"sdsd"
+    })}
+
+    function Figma (){
+        setTextTec({
+            title:"Figma",
+            description:"sdsd"
+    })}
+
+    function GitHub (){
+        setTextTec({
+            title:"GitHub",
+            description:""
+    })}
+
+
+
     return(
         <ContainerBaseCards>
 
             <div className="descriptionTechnologyContainer">
-                <h2>ReactJs</h2>
-                <p>sad</p>
-
+                <h2>{textTec.title}</h2>
+                <div className="pdescriptionContainer">
+                    <h3>{textTec.description}</h3>
+                </div>
+  
             </div>
 
 
             <div className="cardsBase">
-                <div className="cardSlot">{card}</div>
-                <div className="cardSlot2">{card2}</div>
-                <div className="cardSlot3">{card3}</div>
+                <button className="cardSlot" onClick={ReactJs}>{card}</button>
+                <button className="cardSlot2" onClick={Figma}>{card2}</button>
+                <button className="cardSlot3" onClick={GitHub}>{card3}</button>
             </div>
 
 
